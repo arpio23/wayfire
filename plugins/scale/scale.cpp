3f7fe57a5bcdb6a03923b10c944a9cc46ffe8858
Ilia Bozhinov <ammen99@gmail.com>
2023-01-23 14:30:21 +0100

    scale: disconnect view-mapped signal on finalize()

--- plugins/scale/scale.cpp
+++ plugins/scale/scale.cpp
@@ -1418,6 +1418,7 @@ class wayfire_scale : public wf::per_output_plugin_instance_t,
         scale_data.clear();
         grab->ungrab_input();
         view_focused.disconnect();
+        on_view_mapped.disconnect();
         view_unmapped.disconnect();
         view_attached.disconnect();
         view_detached.disconnect();
