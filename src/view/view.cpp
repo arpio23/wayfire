ee75a5e2107d4fcdee4e54c04fd3f9c5d13b4782
Ilia Bozhinov <ammen99@gmail.com>
2023-01-30 18:34:38 +0100

    api: make decorator interface more sensible
    Decorators are asked to implement two functions, when all that we need
    to know about a decoration are the sizes of the margins on each side.

--- src/view/view.cpp
+++ src/view/view.cpp
@@ -367,11 +367,6 @@ void wf::view_interface_t::set_tiled(uint32_t edges)
     data.new_edges = edges;
 
     this->tiled_edges = edges;
-    if (priv->frame)
-    {
-        priv->frame->notify_view_tiled();
-    }
-
     this->emit(&data);
     if (this->get_output())
     {
@@ -389,15 +384,10 @@ void wf::view_interface_t::set_fullscreen(bool full)
     }
 
     fullscreen = full;
-    if (priv->frame)
-    {
-        priv->frame->notify_view_fullscreen();
-    }
 
     view_fullscreen_signal data;
     data.view  = self();
     data.state = full;
-
     if (get_output())
     {
         get_output()->emit(&data);
@@ -408,13 +398,7 @@ void wf::view_interface_t::set_fullscreen(bool full)
 
 void wf::view_interface_t::set_activated(bool active)
 {
-    if (priv->frame)
-    {
-        priv->frame->notify_view_activated(active);
-    }
-
     activated = active;
-
     view_activated_state_signal ev;
     this->emit(&ev);
 }
@@ -721,10 +705,6 @@ void wf::view_interface_t::set_decoration(
         target_wm_geometry = get_output()->workspace->get_workarea();
     }
 
-    // notify the frame of the current size
-    priv->frame->notify_view_resized(get_wm_geometry());
-    // but request the target size, it will be sent to the frame on the
-    // next commit
     set_geometry(target_wm_geometry);
     damage();
 
